eeae9940fc584dd57ddea4e922c76d0b073cf4a9
Syxh0wN <leonardobennr@gmail.com>
2023-04-20 17:29:50 -0300

    feat: Profile

--- src/services/user/login/index.ts
+++ src/services/user/login/index.ts
@@ -4,6 +4,7 @@ import { iUserLogin } from "../../../interfaces/user";
 import { AppDataSource } from "../../../data-source";
 import { User } from "../../../entities/entities/user";
 import { AppError } from "../../../errors/appError";
+import { instanceToPlain } from "class-transformer";
 
 export const createLoginService = async (data: iUserLogin): Promise<Object> => {
   const { email, password } = data;
@@ -16,10 +17,13 @@ export const createLoginService = async (data: iUserLogin): Promise<Object> => {
   const isPasswordValid = await compare(password, user.password);
   if (!isPasswordValid) throw new AppError("User or password invalid!", 403);
 
+  const new_user = instanceToPlain(user);
+
   const token = jwt.sign(
     {
       id: user.id,
       admin: user.admin,
+      user: new_user,
     },
     String(process.env.SECRET_KEY),
     {
@@ -31,5 +35,6 @@ export const createLoginService = async (data: iUserLogin): Promise<Object> => {
     token,
     admin: user.admin,
     id: user.id,
+    user: new_user,
   };
 };
